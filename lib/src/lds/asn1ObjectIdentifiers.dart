017b478f5a12c5d5f0ba977c684a3735f3648364
iamEtornam <sunumacbright@gmail.com>
2024-03-25 07:30:44 +0000

    update packages

--- lib/src/lds/asn1ObjectIdentifiers.dart
+++ lib/src/lds/asn1ObjectIdentifiers.dart
@@ -1,25 +1,22 @@
 // Created by Nejc Skerjanc, copyright © 2023 ZeroPass. All rights reserved.
 
-import 'dart:typed_data';
 import 'package:dmrtd/extensions.dart';
 import 'package:logging/logging.dart';
-import 'package:pointycastle/asn1.dart';
 import 'package:pointycastle/asn1/object_identifiers_database.dart';
-
-import '../../types/exception.dart';
 
 
 // here you can add additional object identifiers that are not defined in pointycastle library
 List<Map<String, Object>> customOIDS = [
-{
-'identifierString': '1.2.3.4.5',  'readableName': 'someName', 'identifier': [0, 1, 2, 3, 4, 5]
-}
+  {
+    'identifierString': '1.2.3.4.5',
+    'readableName': 'someName',
+    'identifier': [0, 1, 2, 3, 4, 5]
+  }
 ];
-
 
 // add additional object identifiers
 
-class ASN1ObjectIdentifierException implements DMRTDException {
+class ASN1ObjectIdentifierException {
   final String message;
   @override
   String exceptionName = 'ASN1ObjectIdentifierException';
@@ -31,21 +28,17 @@ class ASN1ObjectIdentifierException implements DMRTDException {
   //  if (message is String) return '$result: $message';
   //  return result;
   //}
-
-
 }
-
 
 class ASN1ObjectIdentifiers {
   // object identifiers that are defined in pointycastle library
-  List<Map<String, Object>> _OIDS = oi;
+  final List<Map<String, Object>> _OIDS = oi;
   final _log = Logger("ASN1ObjectIdentifiers");
-  
-  
-  ASN1ObjectIdentifiers(){
+
+  ASN1ObjectIdentifiers() {
     // add custom object identifiers to existing ones
     for (var customOID in customOIDS) {
-      if (!checkOID(item:customOID)){
+      if (!checkOID(item: customOID)) {
         throw ASN1ObjectIdentifierException('Object identifier is not valid.');
       }
       _OIDS.add(customOID);
@@ -53,10 +46,13 @@ class ASN1ObjectIdentifiers {
   }
 
   // check if object identifier is valid
-  bool checkOID({required Map<String, Object> item}){
+  bool checkOID({required Map<String, Object> item}) {
     //check if list contains all required keys
-    if (!item.containsKey('identifier') || !item.containsKey('identifierString') || !item.containsKey('readableName')) {
-      _log.error('Object identifier must contain identifier, identifierString and readableName.');
+    if (!item.containsKey('identifier') ||
+        !item.containsKey('identifierString') ||
+        !item.containsKey('readableName')) {
+      _log.error(
+          'Object identifier must contain identifier, identifierString and readableName.');
       return false;
     }
 
@@ -75,17 +71,18 @@ class ASN1ObjectIdentifiers {
     return true;
   }
 
-
   // has object identifier with identifier string
   bool hasOIDWithIdentifierString(String identifierString) {
-    return _OIDS.any((element) => element['identifierString'] == identifierString);
+    return _OIDS
+        .any((element) => element['identifierString'] == identifierString);
   }
-
 
   // get object identifier by identifier string
   Map<String, Object> getOIDByIdentifierString(String identifierString) {
-    return _OIDS.firstWhere((element) => element['identifierString'] == identifierString, orElse: () =>
-      throw ASN1ObjectIdentifierException('Object identifier with identifier string $identifierString does not exist.'));
+    return _OIDS.firstWhere(
+        (element) => element['identifierString'] == identifierString,
+        orElse: () => throw ASN1ObjectIdentifierException(
+            'Object identifier with identifier string $identifierString does not exist.'));
   }
 
   // has object identifier wih identifier
@@ -95,9 +92,8 @@ class ASN1ObjectIdentifiers {
 
   // get object identifier by identifier
   Map<String, Object> getOIDByIdentifier(List<int> identifier) {
-    return _OIDS.firstWhere((element) => element['identifier'] == identifier, orElse: () =>
-      throw ASN1ObjectIdentifierException('Object identifier with identifier $identifier does not exist.'));
+    return _OIDS.firstWhere((element) => element['identifier'] == identifier,
+        orElse: () => throw ASN1ObjectIdentifierException(
+            'Object identifier with identifier $identifier does not exist.'));
   }
-
 }
-
